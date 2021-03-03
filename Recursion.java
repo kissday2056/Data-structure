77431ec35644e00c09dff20b77901a8478c1878c
ming9 <kissday2056@naver.com>
2021-03-03 14:09:27 +0900

    재귀함수 팩토리얼 수정

--- Recursion.java
+++ Recursion.java
@@ -2,10 +2,14 @@ package Structure;
 
 public class Recursion {
 //재귀 호출 
-	static void Hello() {
-		System.out.println("Hello");
+	public static int fact(int n) {
+		if(n <= 1) 
+			return n;
+		else 
+			return fact(n-1) * n;
 	}
 	public static void main(String[] args) {
-		Hello();
+		int what = 4;
+		System.out.println(fact(what));
 	}
 }
